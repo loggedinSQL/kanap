7e854a9578783a1d5203bc2d9f76059bec735b78
themarsiancoder <themarsiancoder@gmail.com4>
2022-09-27 14:40:54 +0200

    ajout de fonction dans le fichier

--- front/js/script.js
+++ front/js/script.js
@@ -8,31 +8,50 @@ fetch("http://localhost:3000/api/products")
             // récupérer l'élément du DOM qui recueillera les fiches produits
             const section = document.querySelector("#items");
 
-            // créer la fiche en récupérant les données du produit
             const link = document.createElement("a");
             link.href = "./product.html?id=" + element._id;
             section.appendChild(link);
-
+            
             const article = document.createElement("article");
             link.appendChild(article);
 
-            const image = document.createElement("img");
-            image.src = element.imageUrl;
-            image.alt = element.altTxt;
-            article.appendChild(image);
-
-            const name = document.createElement("h3");
-            name.classList.add("productName");
-            name.textContent = element.name;
-            article.appendChild(name);
-
-            const description = document.createElement("p");
-            description.classList.add("productDescription");
-            description.textContent = element.description;
-            article.appendChild(description);
+            article.appendChild(createImage(element));
+            article.appendChild(createName(element));
+            article.appendChild(createDescription(element));
         }
     })
     .catch(err => {
         // une erreur est survenue
     })
 
+
+    // ajout de l'image du canape
+    const createImage = (canape) => {
+        const image = document.createElement("img");
+        image.src = canape.imageUrl;
+        image.alt = canape.altTxt;
+    
+        return image;
+    }
+    
+    
+    // ajout du nom de l'article
+    const createName = (item) => {
+        const name = document.createElement("h3");
+        name.classList.add("productName");
+        name.textContent = item.name;
+    
+        return name;
+    }
+    
+    
+    // ajout de la description de l'article
+    const createDescription = (item) => {
+        const description = document.createElement("p");
+        description.classList.add("productDescription");
+        description.textContent = item.description;
+        
+        return description;
+    }
+    
+    
